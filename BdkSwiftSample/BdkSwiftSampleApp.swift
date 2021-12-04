57ae871d9befb98d1b63b0321ebec37c7f891d66
Paul Miller <paul@paul.lol>
2021-12-04 10:45:15 -0600

    Initial Commit

---
+++ BdkSwiftSample/BdkSwiftSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BdkSwiftSampleApp.swift
+//  BdkSwiftSample
+//
+//  Created by Paul Miller on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BdkSwiftSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
